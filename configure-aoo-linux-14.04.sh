61e807aab9bcce65e6c45f0c638d8c4aab8863c6
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2014-08-24 14:38:32 +0200

    [helpers] Added bash script helper files to configure and build AOO under Kubuntu 14.04. Keep note of https://issues.apache.org/ooo/show_bug.cgi?id=125475 while building

---
+++ configure-aoo-linux-14.04.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#solo all'inizio o dopo un update:
+#autoconf
+
+#--with-epm-url="http://www.epmhome.org/software.php?VERSION=4.2&FILE=epm/3.7/epm-3.7-source.tar.bz2"  \
+
+
+#./configure --with-epm-url=http://ftp.easysw.com/pub/epm/3.7/epm-3.7-source.tar.gz \
+#--with-epm-url=http://epm.sourcearchive.com/downloads/3.7-1/epm_3.7.orig.tar.gz \
+./configure \
+--with-epm-url=http://www.msweet.org/files/project2/epm-3.7-source.tar.gz \
+--with-dmake-url=http://dmake.apache-extras.org.codespot.com/files/dmake-4.12.tar.bz2 \
+--enable-category-b \
+--enable-crashdump \
+--with-package-format="deb" \
+--with-lang='en-US it fr de pt-BR' \
+--enable-opengl  \
+--enable-dbus  \
+--enable-gstreamer \
+--enable-pdfimport \
+--enable-hunspell \
+--enable-hyphen \
+--enable-bundled-dictionaries \
+\
+--with-mingwin=i586-mingw32msvc-c++ \
+\
+--enable-verbose \
+--with-build-version="$(date +"%Y-%m-%d %H:%M:%S (%a, %d %b %Y)") - Rev.$(echo $(git svn info) | sed -e 's/^.*Last Changed Rev: //g' -e 's/ .*//g') $(echo git_$(git log -n1 --format=%h))" \
+
+#            --enable-symbols \
+#            --enable-dbgutil \
