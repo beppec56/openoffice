af015410bc3ce1f3702d8880ecff9ac89bd8e062
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2014-10-06 17:03:55 +0200

    [helpers] Again, not compiling graphite

--- configure-aoo-linux-14.04-devel-quick.sh
+++ configure-aoo-linux-14.04-devel-quick.sh
@@ -39,15 +39,9 @@
 --with-package-format="deb" \
 --without-junit \
 --without-stlport \
---with-system-stdlibs \
 --enable-bundled-dictionaries \
 --with-lang='en-US' \
---enable-opengl  \
 --enable-dbus  \
---enable-gstreamer \
---enable-pdfimport \
---enable-wiki-publisher \
---enable-report-builder \
 \
 --enable-verbose \
 --with-build-version="$(date +"%Y-%m-%d %H:%M:%S (%a, %d %b %Y)") - Rev. $(git branch |grep "*" | sed 's/* //g') $(echo git_$(git log -n1 --format=%h))" \
@@ -56,8 +50,6 @@
 --enable-symbols \
 --disable-strip-solver \
 --enable-debug \
---enable-dbgutil \
---with-system-graphite \
 
 
 
