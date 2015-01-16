4a383566ced26d2c828c6aa8dce4a74429495a85
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2015-01-16 09:54:36 +0100

    [ucb] + debug prints again...

--- main/ucb/source/ucp/webdav/webdavcontentcaps.cxx
+++ main/ucb/source/ucp/webdav/webdavcontentcaps.cxx
@@ -61,6 +61,9 @@ using namespace http_dav_ucp;
 bool ContentProvider::getProperty(
         const rtl::OUString & rPropName, beans::Property & rProp, bool bStrict )
 {
+
+  fprintf( stdout, "==\n=====>>>>> ContentProvider::getProperty  %s \n", OUStringToOString( rPropName , RTL_TEXTENCODING_ISO_8859_1 ).getStr());
+
     if ( !m_pProps )
     {
         osl::MutexGuard aGuard( m_aMutex );
