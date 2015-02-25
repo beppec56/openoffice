675fe5357cd7901be04cd338ed26eedab6faf144
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2015-02-25 11:19:33 +0100

    i126305 Add lock functionality to WebDAV, according to RFC4918

--- main/ucb/source/ucp/webdav/SerfUnlockProcImpl.cxx
+++ main/ucb/source/ucp/webdav/SerfUnlockProcImpl.cxx
@@ -22,72 +22,71 @@
 // MARKER(update_precomp.py): autogen include statement, do not remove
 #include "precompiled_ucb.hxx"
 
-#include <rtl/ustring.hxx>
+#include "SerfTypes.hxx"
+#include "SerfUnlockProcImpl.hxx"
+#include "DAVProperties.hxx"
 
-#include "SerfPutReqProcImpl.hxx"
-
+#include "webdavresponseparser.hxx"
 #include <serf/serf.h>
+#include <rtl/ustrbuf.hxx>
+#include <apr/apr_strings.h>
 
 namespace http_dav_ucp
 {
 
-SerfPutReqProcImpl::SerfPutReqProcImpl( const char* inPath,
+SerfUnlockProcImpl::SerfUnlockProcImpl( const char* inSourcePath,
                                         const DAVRequestHeaders& inRequestHeaders,
-                                        const char* inData,
-                                        const char* inLockToken,
-                                        apr_size_t inDataLen )
-    : SerfRequestProcessorImpl( inPath, inRequestHeaders )
-    , mpData( inData )
-    , mpLockToken( inLockToken)
-    , mnDataLen( inDataLen )
+                                        const ucb::Lock& inLock,
+                                        const char* inLockToken )
+    : SerfRequestProcessorImpl( inSourcePath, inRequestHeaders )
+    , mLock( inLock )
+    , mpLockToken( inLockToken )
+    , xInputStream( new SerfInputStream() )
+{
+
+}
+
+SerfUnlockProcImpl::~SerfUnlockProcImpl()
 {
 }
 
-SerfPutReqProcImpl::~SerfPutReqProcImpl()
+serf_bucket_t * SerfUnlockProcImpl::createSerfRequestBucket( serf_request_t * inSerfRequest )
 {
-}
-
-serf_bucket_t * SerfPutReqProcImpl::createSerfRequestBucket( serf_request_t * inSerfRequest )
-{
-    serf_bucket_alloc_t* pSerfBucketAlloc = serf_request_get_alloc( inSerfRequest );
-
-    // create body bucket
-    serf_bucket_t* body_bkt = 0;
-    if ( mpData != 0 && mnDataLen > 0 )
-    {
-        body_bkt = SERF_BUCKET_SIMPLE_STRING_LEN( mpData, mnDataLen, pSerfBucketAlloc );
-    }
-
     // create serf request
     serf_bucket_t *req_bkt = serf_request_bucket_request_create( inSerfRequest,
-                                                                 "PUT",
+                                                                 "UNLOCK",
                                                                  getPathStr(),
-                                                                 body_bkt,
+                                                                 0,
                                                                  serf_request_get_alloc( inSerfRequest ) );
-    handleChunkedEncoding(req_bkt, mnDataLen);
-
     // set request header fields
     serf_bucket_t* hdrs_bkt = serf_bucket_request_get_headers( req_bkt );
-    // general header fields provided by caller
-    setRequestHeaders( hdrs_bkt );
-    if(mpLockToken)
+    if (hdrs_bkt != NULL)
     {
-    // request specific header field
-        serf_bucket_headers_set( hdrs_bkt, "if", mpLockToken );
+        // general header fields provided by caller
+        setRequestHeaders( hdrs_bkt );
+
+        // request specific header fields
+        serf_bucket_headers_set( hdrs_bkt, "Lock-Token", mpLockToken );
+    }
+    else
+    {
+        OSL_ASSERT("Headers Bucket missing");
     }
 
     return req_bkt;
 }
 
-void SerfPutReqProcImpl::processChunkOfResponseData( const char* /*data*/,
-                                                     apr_size_t /*len*/ )
+void SerfUnlockProcImpl::processChunkOfResponseData( const char* data, 
+                                                      apr_size_t len )
 {
-    // nothing to do;
+    if ( xInputStream.is() )
+    {
+        xInputStream->AddToStream( data, len );
+    }
 }
 
-void SerfPutReqProcImpl::handleEndOfResponseData( serf_bucket_t * /*inSerfResponseBucket*/ )
+void SerfUnlockProcImpl::handleEndOfResponseData( serf_bucket_t * /*inSerfResponseBucket*/ )
 {
-    // nothing to do;
 }
 
 } // namespace http_dav_ucp
