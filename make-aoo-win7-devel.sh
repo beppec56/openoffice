4f669b33df51f7a9b5301531d891a734d9c496a9
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2015-03-10 15:41:57 +0100

    [helpers] Adapted for Windows 7, current build machine
    helpers Removed wrong line terminator CRLF =-> LF
    helpers Fixed wrong env script, changed number of cpu used
    helpers Changed some stuff in configuration script for win7
    helpers Some small change to configure and build helpers

---
+++ make-aoo-win7-devel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#set -x
+
+DATESET=`date +%Y%m%d-%H%M%S`
+LOGFILE="make-aoo-$DATESET.log"
+
+date > ../../$LOGFILE
+
+echo 'Grab Environment'
+. ./winenv.set.sh
+
+echo "$SOLARENV"
+
+cd $SOLARSRC/instsetoo_native || exit 1;
+echo "Starting build"
+
+echo "log file: "
+echo "grep \"Building\" ../$LOGFILE ; tail -f ../$LOGFILE | grep \"Building\""
+echo ""
+
+perl $SOLARENV/bin/build.pl --html --all -- -P1 >> ../../$LOGFILE 2>&1 &
+
+echo "see file at:  $SOLARSRC/../log/unxlngx6.pro.build.html"
