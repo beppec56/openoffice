be2b8c82a9576b8c983795ac723620978e5d75ec
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2015-03-13 10:53:41 +0100

    [serf] Adapted 1.3.8 to compile under Linux in AOO build system

--- ext_libraries/serf/makefile.mk
+++ ext_libraries/serf/makefile.mk
@@ -65,6 +65,8 @@ BUILD_FLAGS+= -f ../../../../win/Makefile -j$(EXTMAXPROCESS)
 
 .ELSE
 
+## CDEFS hold the define used by scons system to build
+
 .IF "$(OS)"=="MACOSX" || "$(OS)"=="FREEBSD" || "$(OS)"=="LINUX"
 # Do not link against expat.  It is not necessary (apr-util is already linked against it)
 # and does not work (we use a different expat library schema.)
@@ -79,37 +81,99 @@ BUILD_FLAGS+= -f ../../../../win/Makefile -j$(EXTMAXPROCESS)
 # Export ENABLE_SERF_LOGGING=YES to enable serf logging
 .IF "$(ENABLE_SERF_LOGGING)" == "YES"
 #PATCH_FILES+=$(TARFILE_NAME).logging.patch
-CDEFS+=-DENABLE_SERF_VERBOSE -DSERF_VERBOSE
+#CDEFS+=-DENABLE_SERF_VERBOSE -DSERF_VERBOSE
 .ENDIF
 
 CONFIGURE_DIR=
-CONFIGURE_ACTION=autoconf && .$/configure
+CONFIGURE_ACTION=
+CONFIGURE_FLAGS=
 
 .IF "$(OS)"=="LINUX"
 .IF "$(SYSTEM_OPENSSL)"=="YES"
-CDEFS+=$(OPENSSL_CFLAGS)
+CONFOSLL=$(OPENSSL_CFLAGS)
 .ELSE
 OPENSSLINCDIR=external
-CDEFS+=-I$(SOLARINCDIR)$/$(OPENSSLINCDIR)
+CONFOSLL=$(SOLARINCDIR)$/$(OPENSSLINCDIR)
 .ENDIF
 .ENDIF
 
 # On Linux/Mac we need the content of CDEFS in CFLAGS so that the ssl headers are searched for
 .IF "$(OS)"=="MACOSX" || "$(OS)"=="LINUX"
 # in a directory that corresponds to the directory that is searched for the ssl library.
-CONFIGURE_FLAGS='CFLAGS=$(CDEFS)'
+CONFIGURE_SCONS='CFLAGS=$(CDEFS)'
 .ENDIF
 
+# what can be fed to scons (serf 1.3.8 version)
+# $ scons --help
+# scons: Reading SConscript files ...
+# scons: done reading SConscript files.
+# 
+# PREFIX: Directory to install under ( /path/to/PREFIX )
+#     default: /usr/local
+#     actual: /usr/local
+# 
+# LIBDIR: Directory to install architecture dependent libraries under ( /path/to/LIBDIR )
+#     default: $PREFIX/lib
+#     actual: /usr/local/lib
+# 
+# APR: Path to apr-1-config, or to APR's install area ( /path/to/APR )
+#     default: /usr
+#     actual: /usr
+# 
+# APU: Path to apu-1-config, or to APR's install area ( /path/to/APU )
+#     default: /usr
+#     actual: /usr
+# 
+# OPENSSL: Path to OpenSSL's install area ( /path/to/OPENSSL )
+#     default: /usr
+#     actual: /usr
+# 
+# ZLIB: Path to zlib's install area ( /path/to/ZLIB )
+#     default: /usr
+#     actual: /usr
+# 
+# GSSAPI: Path to GSSAPI's install area ( /path/to/GSSAPI )
+#     default: None
+#     actual: None
+# 
+# DEBUG: Enable debugging info and strict compile warnings (yes|no)
+#     default: False
+#     actual: False
+# 
+# APR_STATIC: Enable using a static compiled APR (yes|no)
+#     default: False
+#     actual: False
+# 
+# CC: Command name or path of the C compiler
+#     default: None
+#     actual: gcc
+# 
+# CFLAGS: Extra flags for the C compiler (space-separated)
+#     default: None
+#     actual: 
+# 
+# LIBS: Extra libraries passed to the linker, e.g. "-l<library1> -l<library2>" (space separated)
+#     default: None
+#     actual: None
+# 
+# LINKFLAGS: Extra flags for the linker (space-separated)
+#     default: None
+#     actual: 
+# 
+# CPPFLAGS: Extra flags for the C preprocessor (space separated)
+#     default: None
+#     actual: None
+
 BUILD_DIR=$(CONFIGURE_DIR)
-BUILD_ACTION=$(GNUMAKE)
+BUILD_ACTION=scons $(CONFIGURE_SCONS) APR=$(SOLARVERSION)/$(INPATH) APU=$(SOLARVERSION)/$(INPATH) OPENSSL=$(CONFOSLL)
 BUILD_FLAGS+= -j$(EXTMAXPROCESS)
 
 .IF "$(OS)"=="MACOSX"
 # Serf names its library only with the major number.
 # We are using minor and micro as well.  Fix that here
 # by creating a copy with the right name.
-SERF_ORIGINAL_LIB=.libs/libserf-$(SERF_MAJOR).0.0.0.dylib
-SERF_FIXED_LIB=.libs/libserf-$(LIBSERFVERSION).0.dylib
+SERF_ORIGINAL_LIB=libserf-$(SERF_MAJOR).0.0.0.dylib
+SERF_FIXED_LIB=libserf-$(LIBSERFVERSION).0.dylib
 INSTALL_ACTION=if [ -f "$(SERF_ORIGINAL_LIB)" -a ! -f "$(SERF_FIXED_LIB)" ]; then cp $(SERF_ORIGINAL_LIB) $(SERF_FIXED_LIB); fi	 
 .ENDIF
 
@@ -118,17 +182,20 @@ OUT2INC_SUBDIR=serf
 
 .IF "$(OS)"=="MACOSX"
 OUT2LIB+=$(SERF_FIXED_LIB)
-OUT2LIB+=.libs/libserf-1.*dylib
+OUT2LIB+=libserf-1.*dylib
 .ELSE
-OUT2LIB=.libs/libserf-1.so*
+OUT2LIB=libserf-1.so*
 .ENDIF
 
 .ENDIF
+
 
 # --- Targets ------------------------------------------------------
 
 .INCLUDE : set_ext.mk
+#prepare target
 .INCLUDE : target.mk
+#build target
 .INCLUDE : tg_ext.mk
 
-.ENDIF
+.ENDIF # .IF "$(OS)"=="WNT"
